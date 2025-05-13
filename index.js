f20276f44f9d6c3da5678858fc158bab9731108c
khulantuwshinbayar <khulantuwshinbayar@gmail.com>
2025-05-13 13:58:43 +0800

    session

--- index.js
+++ index.js
@@ -20,28 +20,28 @@ store.on('error', (error) => {
   console.error('Session store error:', error);
 });
 
-// app.use(
-//   session({
-//     store,
-//     secret: process.env.SESSION_SECRET || 'SESSION_SECRET123',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       secure: process.env.NODE_ENV === 'production',
-//       maxAge: 24 * 60 * 60 * 1000,
-//       httpOnly: true,
-//       sameSite: 'lax',
-//     },
-//   })
-// );
+app.use(
+  session({
+    store,
+    secret: process.env.SESSION_SECRET || 'SESSION_SECRET123',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: 'lax',
+    },
+  })
+);
 
 
 // Routes
 const SessionPlaceRoutes = require("./routes/places");
-const JwtPlaceRoutes = require("./routes/places-jwt");
+// const JwtPlaceRoutes = require("./routes/places-jwt");
 const UserRoutes = require("./routes/users");
 const SessionAuthRoute = require("./routes/auth-session");
-const JwtAuthRoute = require("./routes/auth-jwt");
+// const JwtAuthRoute = require("./routes/auth-jwt");
 
 
 // Middlewares
@@ -57,10 +57,10 @@ app.use(
 app.use(cookieParser());
 
 // Routes
-app.use("/api/places", JwtPlaceRoutes);
+app.use("/api/places", SessionPlaceRoutes);
 app.use("/api/users", UserRoutes);
-// app.use("/api/auth",SessionAuthRoute);
-app.use("/api/auth",JwtAuthRoute);
+app.use("/api/auth",SessionAuthRoute);
+// app.use("/api/auth",JwtAuthRoute);
 
 app.listen(process.env.PORT,"0.0.0.0",()=>{
   console.log(`server started on ${process.env.PORT} PORT🔥`);
